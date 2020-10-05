526356c06816e0d486d3ba18d5e1d03727756fb3
Maksim Storazhau <Thelionblack.com@gmail.com>
2020-10-05 06:19:38 +0300

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = [];
+    let result = '';
+    let length =  expr.length / 10;
+    for(let i = 0; i < length; i++) {
+        if(expr.slice(0, 10) === '**********') {
+            array.push('space');
+        }else {
+            array.push([expr.slice(0, 2), expr.slice(2, 4), expr.slice(4, 6), expr.slice(6, 8), expr.slice(8, 10)]);
+        }
+        expr = expr.slice(10);
+    }
+    array.forEach(elem => {
+        let str = '';
+        if(elem === 'space') {
+            result += ' ';
+        }else {
+            elem.forEach(elem => {
+                if(elem === '10') {
+                    str += '.';
+                }else if(elem === '11') {
+                    str += '-';
+                }
+            });
+            for (let key in MORSE_TABLE){
+                if(key === str) {
+                    result += MORSE_TABLE[key];
+                    str = '';
+                }
+            }
+        }
+    });
+    console.log(result)
+    return result;
 }
 
 module.exports = {
